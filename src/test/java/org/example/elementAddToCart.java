6047ae214031600a03daa1583cccea01671081b3
Rahul Naik <naikrahul661@gmail.com>
2022-12-14 10:51:32 +0530

    synchronization func added

--- src/test/java/org/example/elementAddToCart.java
+++ src/test/java/org/example/elementAddToCart.java
@@ -6,32 +6,39 @@ import org.openqa.selenium.WebElement;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.example.synchronization.Cart;
+
 public class elementAddToCart {
      static WebDriver driver;
 
     public static void main(String[] args) {
 
         webDriver dri = new webDriver();
-        driver=dri.driver();
-        String[] itemNeeded = {"Cucumber", "Brocolli"};
-        List<String> itemNeededList = Arrays.asList(itemNeeded);
+        driver = dri.driver();
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
         driver.manage().window().maximize();
+        addProdToCart(driver);
+        Cart(driver);
+
+
+    }
+
+    public static void addProdToCart(WebDriver driver) {
+        String[] itemNeeded = {"Cucumber", "Brocolli"};
+        List<String> itemNeededList = Arrays.asList(itemNeeded);
         List<WebElement> product = driver.findElements(By.cssSelector("h4[class='product-name']"));
-        int j=0;
-        for (int i=0;i<product.size();i++){
+        int j = 0;
+        for (int i = 0; i < product.size(); i++) {
             String[] name = product.get(i).getText().split("-");
-            String prodName=  name[0].trim();
-            if(itemNeededList.contains(prodName)){
+            String prodName = name[0].trim();
+            if (itemNeededList.contains(prodName)) {
                 j++;
-               driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
             }
-            if(j>itemNeeded.length){
+            if (j > itemNeeded.length) {
                 break;
             }
         }
-
-
     }
 }
--- src/test/java/org/example/synchronization.java
+++ src/test/java/org/example/synchronization.java
@@ -1,13 +1,23 @@
+
+//implicit wait :
+//we define n number of sec; Applicable until present drive terminated; once elem is availble then come out from wait;
+//con:not able to estimate the performance of the web
+// Explicit wait :
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class synchronization {
-    static WebDriver driver;
 
-    public static void main(String[] args) {
-        webDriver dri = new webDriver();
-        driver=dri.driver();
-        
+    public static void Cart(WebDriver driver){
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+        driver.findElement(By.cssSelector("input[class='promoCode']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button[class='promoBtn']")).click();
+
     }
 }
+
+
+
